cefc80e9f3f065342b5d72b3c97e165683c99d8e
Vahidpro <ivahidp@gmail.com>
2024-04-15 16:17:30 +0330

    Added tailwind configuration

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
 	return (
-		<header>
+		<header className="bg-pink-500">
 			<Link to="/">Fast Pizza Co.</Link>
 
 			<SearchOrder />
