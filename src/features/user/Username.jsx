d79ba65cf59ecba7ac2afe9317e1c12b04ef9795
Vahidpro <ivahidp@gmail.com>
2024-04-16 11:27:13 +0330

    Added basic CSS styles with tailwind

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+    return (
+        <div className="text-sm font-semibold">
+            Vahid
+        </div>
+    )
+}
+
+export default Username
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
-	return (
-		<header className="bg-pink-500">
-			<Link to="/">Fast Pizza Co.</Link>
+  return (
+    <>
+      <header className="border-b-2 border-pink-700 bg-pink-500 px-4 py-3 uppercase tracking-widest">
+        <Link to="/">Fast Pizza Co.</Link>
 
-			<SearchOrder />
-		</header>
-	);
+        <SearchOrder />
+
+        <Username />
+      </header>
+    </>
+  );
 }
 
 export default Header;
