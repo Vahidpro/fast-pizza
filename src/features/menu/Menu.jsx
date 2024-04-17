2f06b5634d375a9c4ded5ad0b47d2e2b08a3aedb
Vahidpro <ivahidp@gmail.com>
2024-04-17 17:12:24 +0330

    Applied menu style

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -3,22 +3,19 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
 
 function Menu() {
-	const menu = useLoaderData();
-	return (
-		<ul>
-			{menu.map((pizza) => (
-				<MenuItem
-					key={pizza.id}
-					pizza={pizza}
-				></MenuItem>
-			))}
-		</ul>
-	);
+  const menu = useLoaderData();
+  return (
+    <ul className="divide-y px-3">
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza}></MenuItem>
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
-	const menu = await getMenu();
-	return menu;
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "inline-block rounded-full bg-pink-400 px-4 py-3 font-semibold uppercase tracking-wide transition-colors duration-300 hover:bg-pink-300 disabled:cursor-not-allowed disabled:bg-gray-300";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block rounded-full bg-pink-400 font-semibold uppercase tracking-wide transition-colors duration-300 hover:bg-pink-300 disabled:cursor-not-allowed disabled:bg-gray-300 px-4 py-2";
+
+  const styles = {
+    primary: base + "px-4 py-3 md:px-6 md:py:4",
+    small: base + "px-4 py-2 md:px-5 md:py:2.5 text-sm",
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button className={className} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
