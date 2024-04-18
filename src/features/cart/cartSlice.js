a8a89afd6cb2e25e39a8eb04eec8b1006bb0ffaf
Vahidpro <ivahidp@gmail.com>
2024-04-18 10:07:16 +0330

    Added delete card items and show button accordingly

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -30,16 +30,16 @@ const cartSlice = createSlice({
 
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
-      item.quantitiy++;
-      item.totalPrice = item.quantitiy * item.unitPrice;
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseItemQuantity(state, action) {
       // payload = pizzaId
 
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
-      item.quantitiy--;
-      item.totalPrice = item.quantitiy * item.unitPrice;
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
     },
     clearCart(state) {
       state.cart = [];
@@ -64,3 +64,6 @@ export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
